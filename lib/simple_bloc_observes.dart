059592baa5eaad3922ad1aec1a31d7a0d646173c
Abdelrahman Ashraf <8effort@gmail.com>
2025-12-22 19:20:51 +0200

    edit note done

--- lib/simple_bloc_observes.dart
+++ lib/simple_bloc_observes.dart
@@ -5,12 +5,10 @@ class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    // TODO: implement onEvent
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
     super.onError(bloc, error, stackTrace);
   }
 
@@ -23,7 +21,6 @@ class SimpleBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    // TODO: implement onChange
   }
 
   @override
--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.titleBage, required this.icon});
+  const CustomAppBar({
+    super.key,
+    required this.titleBage,
+    required this.icon,
+    this.onPressed,
+  });
 
   final String titleBage;
   final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(titleBage, style: const TextStyle(fontSize: 28)),
-        CustomSearchIcon(icon: icon),
+        CustomIcon(icon: icon, onPressed: onPressed),
       ],
     );
   }
